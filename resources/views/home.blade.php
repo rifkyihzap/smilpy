adc8dfbb0b49a87fe3a03f8b70b74237beae25d3
rifkyihzap <rifkyihzap@gmail.com>
2022-12-19 10:09:44 +0700

    second commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,5 +6,6 @@
 <h7 class="card-title fw-bold"> {{ $lokasi }}</h7>
 <p class="card-text text-secondary">Berjarak {{ $jarak }} <br> {{ $tanggal }} </p>
 <a href="/jimbaran" class="card-text fw-bold">{{ $harga }}</a>
-<p>test github</p>
+<p>test github a</p>
+<h1>test github</h1>
 @endsection
