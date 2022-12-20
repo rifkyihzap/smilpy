63c5149312b7798f2d38e68385805102abc76aac
rifkyihzap <rifkyihzap@gmail.com>
2022-12-20 08:10:33 +0700

    MVC

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,67 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Post
 {
-    use HasFactory;
+    private static $title_post = [
+        [
+            "title" => "home a",
+            "lokasi" => "Abiansemal,Indonesia",
+            "slug" => "abiansemal-indonesia",
+            "jarak" => " 309 Kilometer",
+            "tanggal" => "21-26 Agustus",
+            "harga" => "Rp.5.905.265",
+            "image" => "aa.jpg",
+
+        ],
+        [
+            "title" => "home b",
+            "lokasi" => "Singaraja, Indonesia",
+            "slug" => "singaraja-indonesia",
+            "jarak" => " 326 kilometer",
+            "tanggal" => "7-14 Februari",
+            "harga" => "Rp6.747.466",
+            "image" => "bb.jpg"
+
+        ],
+        [
+            "title" => "home c",
+            "lokasi" => "Kecamatan Ubud, Bali, Indonesia",
+            "slug" => "kecamatan-ubud-bali-indonesia",
+            "jarak" => " 313 kilometer",
+            "tanggal" => "6-12 Desember",
+            "harga" => "Rp4.595.400",
+            "image" => "cc.jpg"
+
+        ],
+        [
+            "title" => "home d",
+            "lokasi" => "Kecamatan Kuta Selatan, Bali, Indonesia",
+            "slug" => "kecamatan-kuta-selatan-bali-indonesia",
+            "jarak" => " 310 kilometer",
+            "tanggal" => "15-19 Juni",
+            "harga" => "Rp3.797.500",
+            "image" => "dd.jpg"
+
+        ]
+    ];
+
+    public static function all()
+    {
+        return self::$title_post;
+    }
+
+    public static function find($slug)
+    {
+        $posts = self::$title_post;
+        $post = [];
+        foreach ($posts as $p) {
+            if ($p["slug"] === $slug) {
+                $post = $p;
+            }
+        }
+
+        return $post;
+    }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,5 +6,4 @@
     <h7 class="card-title fw-bold"> {{ $post ["lokasi"] }}</h7>
     <p class="card-text text-secondary">Berjarak {{ $post ["jarak"] }} <br> {{ $post ["tanggal"] }} </p>
     <dt class="card-text fw-bold">{{ $post ["harga"] }} <small class="text-muted">Malam</small></dt>
-    </a>
 @endsection
