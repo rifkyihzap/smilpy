c640c1dcb833739f39af5b9b6cca5ef2e9047677
rifkyihzap <rifkyihzap@gmail.com>
2022-12-20 08:10:04 +0700

    MVC Berhasil

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,112 +15,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $post_title = [
-        [
-            "title" => "home a",
-            "lokasi" => "Abiansemal,Indonesia",
-            "slug" => "abiansemal-indonesia",
-            "jarak" => " 309 Kilometer",
-            "tanggal" => "21-26 Agustus",
-            "harga" => "Rp.5.905.265",
-            "image" => "aa.jpg",
-
-        ],
-        [
-            "title" => "home b",
-            "lokasi" => "Singaraja, Indonesia",
-            "slug" => "singaraja-indonesia",
-            "jarak" => " 326 kilometer",
-            "tanggal" => "7-14 Februari",
-            "harga" => "Rp6.747.466",
-            "image" => "bb.jpg"
-
-        ],
-        [
-            "title" => "home c",
-            "lokasi" => "Kecamatan Ubud, Bali, Indonesia",
-            "slug" => "kecamatan-ubud-bali-indonesia",
-            "jarak" => " 313 kilometer",
-            "tanggal" => "6-12 Desember",
-            "harga" => "Rp4.595.400",
-            "image" => "cc.jpg"
-
-        ],
-        [
-            "title" => "home d",
-            "lokasi" => "Kecamatan Kuta Selatan, Bali, Indonesia",
-            "slug" => "kecamatan-kuta-selatan-bali-indonesia",
-            "jarak" => " 310 kilometer",
-            "tanggal" => "15-19 Juni",
-            "harga" => "Rp3.797.500",
-            "image" => "dd.jpg"
-
-        ]
-    ];
 
     return view('home', [
         "title" => "home",
 
-        "posts" => $post_title
+        "posts" => Post::all()
 
     ]);
 });
 
 //single post//
 Route::get('posts/{slug}', function ($slug) {
-    $post_title = [
-        [
-            "title" => "home a",
-            "lokasi" => "Abiansemal,Indonesia",
-            "slug" => "abiansemal-indonesia",
-            "jarak" => " 309 Kilometer",
-            "tanggal" => "21-26 Agustus",
-            "harga" => "Rp.5.905.265",
-            "image" => "aa.jpg",
 
-        ],
-        [
-            "title" => "home b",
-            "lokasi" => "Singaraja, Indonesia",
-            "slug" => "singaraja-indonesia",
-            "jarak" => " 326 kilometer",
-            "tanggal" => "7-14 Februari",
-            "harga" => "Rp6.747.466",
-            "image" => "bb.jpg"
-
-        ],
-        [
-            "title" => "home c",
-            "lokasi" => "Kecamatan Ubud, Bali, Indonesia",
-            "slug" => "kecamatan-ubud-bali-indonesia",
-            "jarak" => " 313 kilometer",
-            "tanggal" => "6-12 Desember",
-            "harga" => "Rp4.595.400",
-            "image" => "cc.jpg"
-
-        ],
-        [
-            "title" => "home d",
-            "lokasi" => "Kecamatan Kuta Selatan, Bali, Indonesia",
-            "slug" => "kecamatan-kuta-selatan-bali-indonesia",
-            "jarak" => " 310 kilometer",
-            "tanggal" => "15-19 Juni",
-            "harga" => "Rp3.797.500",
-            "image" => "dd.jpg"
-
-        ]
-    ];
-
-    $new_post = [];
-    foreach ($post_title as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
 
     return view('post', [
         "title" => "single post",
-        "post" => $new_post
+        "post" => Post::find($slug)
     ]);
 });
 
